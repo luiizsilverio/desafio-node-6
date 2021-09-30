02a8d60f4c30ac65bc27d48240c97b43dfe770ad
luiizsilverio <luiiz.silverio@gmail.com>
2021-09-30 15:04:48 -0300

    Desafio Testes Unitários

--- src/modules/statements/useCases/createStatement/CreateStatementController.ts
+++ src/modules/statements/useCases/createStatement/CreateStatementController.ts
@@ -12,6 +12,7 @@ export class CreateStatementController {
   async execute(request: Request, response: Response) {
     const { id: user_id } = request.user;
     const { amount, description } = request.body;
+    console.log(request.body)
 
     const splittedPath = request.originalUrl.split('/')
     const type = splittedPath[splittedPath.length - 1] as OperationType;
